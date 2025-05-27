55d7288c0d9c1f948195d56ac4d944c4c8c06634
Matthieu <matthieu.chambon44@gmail.com>
2025-05-27 18:51:07 +0200

    Ajout modèle Contributor

--- SoftDesk/urls.py
+++ SoftDesk/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import ProjectViewSet, IssueViewSet, CommentViewSet
+from api.views import UserViewSet, ContributorViewSet, ProjectViewSet, IssueViewSet, CommentViewSet
 
 
 router = routers.SimpleRouter()
+router.register('user', UserViewSet, basename='user')
+router.register('contributor', ContributorViewSet, basename='contributor')
 router.register('project', ProjectViewSet, basename='project')
 router.register('issue', IssueViewSet, basename='issue')
 router.register('comment', CommentViewSet, basename='comment')
