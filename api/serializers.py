d02c16414274987871969ed75724c7e17cde51d4
Matthieu <matthieu.chambon44@gmail.com>
2025-06-03 17:58:18 +0200

    Suppression endpoint Contributor + Mise à jours affichage des données

--- api/serializers.py
+++ api/serializers.py
@@ -1,70 +1,57 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from .models import User, Contributor, Project, Issue, Comment
+from .models import User, Project, Issue, Comment
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'age', 'can_be_contacted', 'can_data_be_shared']
-        
+
 
 class UserSummarySerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
-        
 
-class ContributorSerializer(ModelSerializer):
-    class Meta:
-        model = Contributor
-        fields = ['id', 'user', 'project']
-        
 
-class ContributorDetailSerializer(ModelSerializer):
-    user = UserSummarySerializer()
-    project = serializers.SerializerMethodField()
-    
+class UserCreateSerializer(ModelSerializer):
     class Meta:
-        model = Contributor
-        fields = ['id', 'user', 'project']
-        
-    def get_project(self, instance):
-        project = instance.project
-        serializer = ProjectSerializer(project)
-        return serializer.data
+        model = User
+        fields = ['username', 'password', 'age', 'can_be_contacted', 'can_data_be_shared']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class ProjectSerializer(ModelSerializer):
-    contributors = serializers.SerializerMethodField()
-    
     class Meta:
         model = Project
-        fields = ['id', 'author', 'contributors', 'title', 'description', 'created_time']
-        
-    def get_contributors(self, instance):
-        contributor_ids = list(
-            instance.contributors.values_list('user__id', flat=True)
-        )
-        return contributor_ids
+        fields = ['id', 'author', 'title', 'description', 'created_time']
+        read_only_fields = ['id', 'author']
 
 
 class ProjectDetailSerializer(ModelSerializer):
     issues = serializers.SerializerMethodField()
     contributors = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Project
         fields = ['id', 'author', 'contributors', 'title', 'description', 'created_time', 'issues']
-        
+
     def get_issues(self, instance):
         queryset = instance.issues.all()
         serializer = IssueSerializer(queryset, many=True)
         return serializer.data
 
     def get_contributors(self, instance):
-        users = User.objects.filter(contributed_projects__project=instance)
+        users = User.objects.filter(contributions__project=instance)
         serializer = UserSummarySerializer(users, many=True)
         return serializer.data
 
@@ -72,34 +59,35 @@ class ProjectDetailSerializer(ModelSerializer):
 class IssueSerializer(ModelSerializer):
     class Meta:
         model = Issue
-        fields = ['id', 'author', 'project', 'title', 'description', 'priority', 'type', 'status', 'created_time']
+        fields = ['id', 'author', 'project', 'title', 'description',
+                  'priority', 'type', 'status', 'created_time']
+        read_only_fields = ['id', 'project']
+
+    # Vérifier si l'auteur de l'issue est un contributeur du projet
+    def validate_author(self, value):
+        project_pk = self.context['view'].kwargs.get('project_pk')
+        project = get_object_or_404(Project, pk=project_pk)
+        if not project.contributors.filter(user=value).exists():
+            raise serializers.ValidationError("L'auteur de l'issue doit être un contributeur du projet.")
+        return value
 
 
 class IssueDetailSerializer(ModelSerializer):
-    author = UserSummarySerializer()
-    project = ProjectSerializer()
     comments = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Issue
-        fields = ['id', 'author', 'project', 'title', 'description', 'priority', 'type', 'status', 'created_time', 'comments']
+        fields = ['id', 'author', 'project', 'title', 'description',
+                  'priority', 'type', 'status', 'created_time', 'comments']
 
     def get_comments(self, instance):
         queryset = instance.comments.all()
         serializer = CommentSerializer(queryset, many=True)
         return serializer.data
-        
+
 
 class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
         fields = ['uuid', 'author', 'issue', 'description', 'created_time']
-        
-
-class CommentDetailSerializer(ModelSerializer):
-    author = UserSummarySerializer()
-    issue = IssueSerializer()
-    
-    class Meta:
-        model = Comment
-        fields = ['uuid', 'author', 'issue', 'description', 'created_time']
+        read_only_fields = ['uuid', 'author', 'issue']
--- api/views.py
+++ api/views.py
@@ -1,49 +1,92 @@
-from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
-from .models import User, Contributor, Project, Issue, Comment
+from .models import User, Project, Issue, Comment
 from api.serializers import (
-    UserSerializer,
-    ContributorSerializer, ContributorDetailSerializer,
+    UserSerializer, UserSummarySerializer, UserCreateSerializer,
     ProjectSerializer, ProjectDetailSerializer,
     IssueSerializer, IssueDetailSerializer,
-    CommentSerializer, CommentDetailSerializer
+    CommentSerializer,
 )
+
+from rest_framework.permissions import IsAuthenticated
+from api.permissions import ProjectPermission, UserPermission, IssueAndCommentPermission
 
 
 class MultipleSerializerMixin:
     detail_serializer_class = None
 
     def get_serializer_class(self):
-        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+        if self.action in ['retrieve'] and self.detail_serializer_class is not None:
             return self.detail_serializer_class
+
         return super().get_serializer_class()
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
+    serializer_class = UserSummarySerializer
+    detail_serializer_class = UserSerializer
+    permission_classes = [UserPermission]
 
-class ContributorViewSet(MultipleSerializerMixin, ModelViewSet):
-    queryset = Contributor.objects.all()
-    serializer_class = ContributorSerializer
-    detail_serializer_class = ContributorDetailSerializer
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'update', 'partial_update']:
+            return self.detail_serializer_class
+        if self.action == 'create':
+            return UserCreateSerializer
+        return super().get_serializer_class()
 
 
 class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     detail_serializer_class = ProjectDetailSerializer
+    permission_classes = [IsAuthenticated, ProjectPermission]
+
+    def perform_create(self, serializer):
+        author = self.request.user
+        serializer.save(author=author)
+
+    @action(detail=True, methods=['post'])
+    def add_contributor(self, request, pk):
+        user = get_object_or_404(User, id=request.data.get('user'))
+        response = self.get_object().add_contributor(user)
+        if response:
+            return Response({'status': 'Utilisateur ajouté en contributeur'}, status=201)
+        return Response({'status': 'Utilisateur déjà contributeur'}, status=400)
+
+    @action(detail=True, methods=['post'])
+    def remove_contributor(self, request, pk):
+        user = get_object_or_404(User, id=request.data.get('user'))
+        response = self.get_object().remove_contributor(user)
+        if response:
+            return Response({'status': 'Utilisateur retiré des contributeurs'}, status=200)
+        return Response({'status': 'Utilisateur n\'est pas contributeur'}, status=400)
 
 
 class IssueViewSet(MultipleSerializerMixin, ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
     detail_serializer_class = IssueDetailSerializer
-    
+    permission_classes = [IsAuthenticated, IssueAndCommentPermission]
+
+    def get_queryset(self):
+        project_pk = self.kwargs.get('project_pk')
+        return self.queryset.filter(project__id=project_pk)
+
+    def perform_create(self, serializer):
+        project = get_object_or_404(Project, id=self.kwargs.get('project_pk'))
+        serializer.save(project=project)
+
 
 class CommentViewSet(MultipleSerializerMixin, ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    detail_serializer_class = CommentDetailSerializer
+    permission_classes = [IsAuthenticated, IssueAndCommentPermission]
+
+    def perform_create(self, serializer):
+        issue = get_object_or_404(Issue, id=self.kwargs.get('issue_pk'))
+        author = self.request.user
+        serializer.save(issue=issue, author=author)
