4fddcb679e0d774c5fc4e868edceee102355b264
Matthieu <matthieu.chambon44@gmail.com>
2025-06-05 13:11:56 +0200

    Mise à jour script remplissage de la base de données

--- populate.py
+++ populate.py
@@ -1,24 +1,20 @@
 import os
 import django
 import random
-import uuid
 from faker import Faker
-from django.utils.timezone import now
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoftDesk.settings')
 django.setup()
 
-from api.models import User, Contributor, Project, Issue, Comment
+from api.models import User, Contributor, Project, Issue, Comment  # noqa: E402
 
 fake = Faker('fr_FR')
 
+
 def run():
-    Comment.objects.all().delete()
-    Issue.objects.all().delete()
-    Project.objects.all().delete()
-    Contributor.objects.all().delete()
-    User.objects.all().delete()
-    
+    call_command('flush', '--noinput')
+
     User.objects.create_superuser(
         username='admin',
         password='admin',
@@ -31,7 +27,7 @@ def run():
     for _ in range(5):
         user = User.objects.create_user(
             username=fake.user_name(),
-            password='user',
+            password='password',
             age=random.randint(18, 60),
             can_be_contacted=fake.boolean(),
             can_data_be_shared=fake.boolean(),
@@ -42,48 +38,68 @@ def run():
         "Application de gestion",
         "Plateforme collaborative",
         "Outil de planification",
-        "Interface utilisateur moderne",
-        "API de suivi de bugs",
-        "Dashboard analytique",
     ]
-    
+
     projects = []
-    for _ in range(3):
-        author = random.choice(users)
+    for i in range(3):
+        author = users[i]
         project = Project.objects.create(
             author=author,
-            title = random.choice(project_titles),
+            title=project_titles[i],
             description=random.choice(['BACKEND', 'FRONTEND', 'IOS', 'ANDROID']),
         )
         projects.append(project)
 
+    contributors = []
+    for project in projects:
+        potential_contributors = [user for user in users if user != project.author]
+        for _ in range(random.randint(1, 3)):
+            contributor = Contributor.objects.create(
+                user=random.choice(potential_contributors),
+                project=project
+            )
+            potential_contributors.remove(contributor.user)
+            contributors.append(contributor)
+
+    issue_titles = [
+        "Problème de performance",
+        "Amélioration de l'interface utilisateur",
+        "Correction de bug",
+        "Ajout de fonctionnalité",
+        "Mise à jour de la documentation",
+    ]
+
     issues = []
-    for _ in range(10):
-        project = random.choice(projects)
-        author = random.choice(users)
-        issue = Issue.objects.create(
-            project=project,
-            author=author,
-            title=fake.sentence(nb_words=4),
-            description=fake.paragraph(nb_sentences=2),
-            priority=random.choice(['LOW', 'MEDIUM', 'HIGH']),
-            type=random.choice(['BUG', 'FEATURE', 'TASK']),
-            status=random.choice(['TODO', 'IN_PROGRESS', 'FINISHED']),
-        )
+    for project in projects:
+        for i in range(random.randint(3, 5)):
+            author = random.choice(project.contributors.all()).user
+            issue = Issue.objects.create(
+                project=project,
+                author=author,
+                title=issue_titles[i],
+                description=fake.paragraph(nb_sentences=2),
+                priority=random.choice(['LOW', 'MEDIUM', 'HIGH']),
+                type=random.choice(['BUG', 'FEATURE', 'TASK']),
+                status=random.choice(['TODO', 'IN_PROGRESS', 'FINISHED']),
+            )
         issues.append(issue)
 
-    for _ in range(15):
-        issue = random.choice(issues)
-        author = random.choice(users)
-        Comment.objects.create(
-            issue=issue,
-            author=author,
-            description=fake.text(max_nb_chars=200),
-            uuid=uuid.uuid4(),
-            created_time=now()
-        )
+    for issue in issues:
+        for _ in range(random.randint(3, 10)):
+            author = random.choice(issue.project.contributors.all()).user
+            Comment.objects.create(
+                issue=issue,
+                author=author,
+                description=fake.text(max_nb_chars=200),
+            )
+
 
 if __name__ == '__main__':
     # > python populate.py
+    print("Insertion des données de test dans la base de données...")
     run()
-    print("Données de test insérées !")
+    print("Données de test insérées !\n")
+    print("Liste des faux utilisateurs créés :")
+    print("ID : 1, username : 'admin', password : 'admin'")
+    for user in User.objects.filter(is_superuser=False):
+        print(f"ID : {user.id}, username : '{user.username}', password : 'password'")
